d71f6088ca826590ff8415b70d578a3928a1bce2
Everett <everett.bowline@gmail.com>
2018-07-18 00:07:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,14 @@ class App < Sinatra::Base
     erb :user_input
   end
 
+<<<<<<< HEAD
   post '/piglatinize' do
     piglatin = PigLatinizer.new
     @latinized_text = piglatin.piglatinize(params[:user_phrase])
+=======
+  post '/piglatinizer' do
+    @latinized_text = PigLatinizer.new(params[:user_text])
+>>>>>>> cfa86879e75715cd5d2156d5b0642b8626de73dd
     
     erb :results
   end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,4 +1,5 @@
 class PigLatinizer
+<<<<<<< HEAD
   
   def piglatinize(text)
     piglatin = ''
@@ -21,15 +22,34 @@ class PigLatinizer
       elsif consonant?(text[0]) && consonant?(text[1]) && consonant?(text[2])
         text = text.slice(3..-1) + text.slice(0,3)
       elsif consonant?(text[0]) && consonant?(text[1])
+=======
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def consonant?(text)
+    !text.scan(/[aAeEiIoOuU]/)
+  end
+  
+  def latinize
+    if !consonant(text[0])
+      text = text + "w"
+      elsif consonant(text[0]) && consonant(text[1])
+>>>>>>> cfa86879e75715cd5d2156d5b0642b8626de73dd
         text = text.slice(2..-1) + text.slice(0,2)
       else
         text = text.slice(1..-1) + text.slice(0)
       end
     text << "ay"
   end
+<<<<<<< HEAD
   
   def piglatinize_sentence(text)
     text.split.collect { |word| piglatinize_word(word) }.join(" ")
   end
   
+=======
+>>>>>>> cfa86879e75715cd5d2156d5b0642b8626de73dd
 end
